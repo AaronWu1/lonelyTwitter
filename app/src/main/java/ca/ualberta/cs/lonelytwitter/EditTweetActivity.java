08679a93fa60603b09a4de106b8da990e2c3b719
AaronWu1 <wyt1994@live.com>
2018-03-02 16:07:45 -0700

    lab7

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/EditTweetActivity.java
@@ -0,0 +1,17 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Aaron on 3/2/2018.
+ */
+
+public class EditTweetActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_tweet);
+    }
+}
